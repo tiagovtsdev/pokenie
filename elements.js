bed8995e277bc1f4d24debeec97ddd744b38198f
Deivid Bobato <bobato.dede@gmail.com>
2026-09-14 20:42:23 -0300

    Adicioneio o array dos pokemons

--- elements.js
+++ elements.js
@@ -2,67 +2,28 @@
 
 const listaPokemons = [
   {
-    "id":25,
-    "name":"pikachu",
-    "height":4,
-    "weight":60,
+   "id":94,
+    "name":"gengar", 
     "sprites": {
-      "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
-    },
-    "types":[
-      {
-        "slot":1,
-        "type":{
-          "name":"electric",
-          "url":"https://pokeapi.co/api/v2/type/13/"
-        }
-      }
-    ]
+      "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png"
+    }
   },
 
   {
-    "id":6,
-    "name":"charizard",
-    "height":17,
-    "weight":905,
+    "id":133,
+    "name":"eevee",
     "sprites": {
-      "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
-    },
-    "types":[
-      {
-        "slot":1,
-        "type":{
-          "name":"fire",
-          "url":"https://pokeapi.co/api/v2/type/10/"
-        }
-      },
-      {
-        "slot":2,
-        "type":{
-          "name":"flying",
-          "url":"https://pokeapi.co/api/v2/type/3/"
-        }
-      }
-    ]
+      "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png"
+    }
+  
   },
 
   {
-    "id":150,
-    "name":"mewtwo",
-    "height":20,
-    "weight":1220,
+    "id":3,
+    "name":"venusaur",
     "sprites": {
-      "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png"
-    },
-    "types":[
-      {
-        "slot":1,
-        "type":{
-          "name":"psychic",
-          "url":"https://pokeapi.co/api/v2/type/14/"
-        }
-      }
-    ]
+      "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"
+    }
   }
 ]
 
@@ -71,10 +32,6 @@ const container = document.getElementById("listaDePokemons");
 listaPokemons.forEach(function(pokemon){
   console.log(pokemon)
 
-  const tiposDePokemon = pokemon.types.map(function(t) {
-  return t.type.name;
-}).join(", ")
-
   const imagem = pokemon.sprites.front_default
 
   const cardHTML = `
@@ -82,9 +39,6 @@ listaPokemons.forEach(function(pokemon){
       <h3>Id: ${pokemon.id}</h3>
       <img src="${imagem}">
       <h2>${pokemon.name}</h2>
-      <p>Height: ${pokemon.height}</p>
-      <p>Weight: ${pokemon.weight}</p>
-      <p><b>Type:</b> ${tiposDePokemon}</p>
     </div> 
   `;
 
