95fcfa4fd2b887a8b442b7029ec6af82ae099529
M7-dev0 <mauricio.hl.santana@gmail.com>
2026-09-14 00:19:44 -0300

    Adicionei o array de 3 objetos(pokemons)

---
+++ elements.js
@@ -0,0 +1,58 @@
+// 1 array com 3 objetos
+
+const listaPokemons = [
+  {
+    "id":25,
+    "name":"pikachu",
+    "height":4,
+    "weight":60,
+    "types":[
+      {
+        "slot":1,
+        "type":{
+          "name":"electric",
+          "url":"https://pokeapi.co/api/v2/type/13/"
+        }
+      }
+    ]
+  },
+
+  {
+    "id":6,
+    "name":"charizard",
+    "height":17,
+    "weight":905,
+    "types":[
+      {
+        "slot":1,
+        "type":{
+          "name":"fire",
+          "url":"https://pokeapi.co/api/v2/type/10/"
+        }
+      },
+      {
+        "slot":2,
+        "type":{
+          "name":"flying",
+          "url":"https://pokeapi.co/api/v2/type/3/"
+        }
+      }
+    ]
+  },
+
+  {
+    "id":150,
+    "name":"mewtwo",
+    "height":20,
+    "weight":1220,
+    "types":[
+      {
+        "slot":1,
+        "type":{
+          "name":"psychic",
+          "url":"https://pokeapi.co/api/v2/type/14/"
+        }
+      }
+    ]
+  }
+]
